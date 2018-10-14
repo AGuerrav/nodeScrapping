214a586f27a40d09e7168fefd44f7ea91d34e340
AGuerrav <aaguerravaldes@gmail.com>
2018-10-14 17:46:19 -0300

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+const request = require('request');
+
+request ('https://www.reddit.com', (err, res, body)=>{
+  if (!err && res.statusCode == 200) {
+    let $ = cheerio.load(body);
+    $('a.dQcRmW','.fRrLNy').each(function() {
+      var urlImg = $(this).attr('href');
+      console.log(urlImg);
+    })
+
+  }
+})
